6f8877b7851035f3a9d648cdd99fbd7dd84a9999
aimarsg <aimar.s.737@gmail.com>
2022-03-17 11:21:28 +0100

    nuevas clases

---
+++ src/packModelo/PortaAviones.java
@@ -0,0 +1,9 @@
+package packModelo; 
+public class PortaAviones extends Barco {
+
+	public void colocarBarco() {
+		// TODO - implement PortaAviones.colocarBarco
+		throw new UnsupportedOperationException();
+	}
+
+}
