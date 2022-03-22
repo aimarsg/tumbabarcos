0e420f5e3e9c361ba89444b9ce81da463c4389ac
aimarsg <aimar.s.737@gmail.com>
2022-03-22 11:45:36 +0100

    el modelo

---
+++ src/packModelo/Modelo.java
@@ -0,0 +1,36 @@
+package packModelo;
+
+import java.util.ArrayList;
+import java.util.Observable;
+
+public class Modelo extends Observable {
+
+	private ArrayList<Flota> flotas;
+	private Modelo miModelo;
+
+	public void jugar() {
+		// TODO - implement Modelo.jugar
+		throw new UnsupportedOperationException();
+	}
+
+	private void inicializar() {
+		// TODO - implement Modelo.inicializar
+		throw new UnsupportedOperationException();
+	}
+
+	public Modelo getModelo() {
+		// TODO - implement Modelo.getModelo
+		throw new UnsupportedOperationException();
+	}
+
+	private Modelo() {
+		// TODO - implement Modelo.Modelo
+		throw new UnsupportedOperationException();
+	}
+
+	private void operation() {
+		// TODO - implement Modelo.operation
+		throw new UnsupportedOperationException();
+	}
+
+}
