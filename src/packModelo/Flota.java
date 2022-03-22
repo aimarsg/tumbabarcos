8a3c2eb4eb592ebaa4bcb973404e9b9ded1a7a8a
aimarsg <aimar.s.737@gmail.com>
2022-03-22 12:16:54 +0100

    cambios de flota

--- src/packModelo/Flota.java
+++ src/packModelo/Flota.java
@@ -10,6 +10,7 @@ public class Flota {
 	private Double presupuesto;
 	private int numBarcos;
 	private ArrayList<Barco> listaBarcos;
+	private ArrayList<Barco> barcosColocados;
 	
 	public Flota() {
 		// TODO - implement Flota.Flota
@@ -21,14 +22,35 @@ public class Flota {
 		throw new UnsupportedOperationException();
 	}
 
-	private void colocarBarcos() {
-		// TODO - implement Flota.colocarBarcos
-		throw new UnsupportedOperationException();
+	public void colocarBarcos(Coordenada pCord, String pTipo, boolean horizontal) {	
+		Barco nuevo =this.obtenerBarco(pTipo);
+		if (nuevo!=null) {
+				if (nuevo.colocarBarco(pCord, horizontal, tablero)){
+					listaBarcos.remove(nuevo);
+					barcosColocados.add(nuevo);
+				}else {
+					System.out.println("El barco no se puede colocar en esa posicion");
+				}
+			
+		}else {
+			//no existe el barco 
+			System.out.println("No existe el barco");
+			
+		}
+		
 	}
 
 	public void inicializarFlota() {
-		// TODO - implement Flota.inicializarFlota
-		throw new UnsupportedOperationException();
+		listaBarcos.add(BarcoFactory.getBarcoFactory().crearBarco("PortaAviones"));
+		listaBarcos.add(BarcoFactory.getBarcoFactory().crearBarco("Submarino"));
+		listaBarcos.add(BarcoFactory.getBarcoFactory().crearBarco("Submarino"));
+		listaBarcos.add(BarcoFactory.getBarcoFactory().crearBarco("Destructor"));
+		listaBarcos.add(BarcoFactory.getBarcoFactory().crearBarco("Destructor"));
+		listaBarcos.add(BarcoFactory.getBarcoFactory().crearBarco("Destructor"));
+		listaBarcos.add(BarcoFactory.getBarcoFactory().crearBarco("Fragata")); 
+		listaBarcos.add(BarcoFactory.getBarcoFactory().crearBarco("Fragata"));
+		listaBarcos.add(BarcoFactory.getBarcoFactory().crearBarco("Fragata"));
+		listaBarcos.add(BarcoFactory.getBarcoFactory().crearBarco("Fragata"));
 	}
 
 	/**
@@ -36,8 +58,25 @@ public class Flota {
 	 * @param Coordenada
 	 */
 	private Barco buscarBarco(Coordenada pcord ) {
-		// TODO - implement Flota.buscarBarco
-		throw new UnsupportedOperationException();
+		int x = pcord.getX();
+		int y = pcord.getY();
+		
+		return null;
+	}
+	public Barco obtenerBarco(String pTipo){
+		Iterator<Barco> it= this.obtIterador();
+		Barco nuevo= null;
+		boolean enc=false;
+		while(it.hasNext()&& !enc){
+			nuevo= it.next();
+			if(nuevo.esBarco(pTipo)){
+				enc=true;
+			}
+		}
+		if(!enc){
+			nuevo=null;
+		}
+		return nuevo;
 	}
 
 	private Iterator<Barco> obtIterador() {
