cf8fb2f2e4777dfddfedce7a400d8142b5aa3676
Mario <Mario@10.57.2.42>
2022-03-22 11:28:29 +0100

    fragata

--- src/packModelo/Fragata.java
+++ src/packModelo/Fragata.java
@@ -4,10 +4,11 @@ public class Fragata extends Barco {
 	
 
 	public void colocarBarco(Coordenada pCoordenada, boolean pHorizontal, Casilla[][] pTablero) {
-	int x= pCoordenada.getX();
-	int y= pCoordenada.getY();
+		int x= pCoordenada.getX();
+		int y= pCoordenada.getY();
 	
-	pTablero[x][y].ColocarBarco();
+		pTablero[x][y].cambiarEstado("Barco");
+		
 	}
 
 }
