b0549a5ac6676096be281fe9080c42f40213190f
Adrian <cuadron11@gmail.com>
2022-03-22 12:42:33 +0100

    submarino

--- src/packModelo/Submarino.java
+++ src/packModelo/Submarino.java
@@ -3,20 +3,19 @@ public class Submarino extends Barco {
 	public Submarino(int numLibres, boolean hundido, String pNom){
 		super(numLibres,hundido,pNom);
 	}
-	public void colocarBarco(Coordenada pCoord, boolean pHorizontal, Casilla[][] pTablero) {
+	public boolean colocarBarco(Coordenada pCoord, boolean pHorizontal, Casilla[][] pTablero) {
 		// TODO - implement Submarino.colocarBarco
-		throw new UnsupportedOperationException();
-		if (pHorizontal) {
-			for (int i=0;i<3; i++) {
-				if (pHorizontal) {
-					pTablero[pCoord.getX()+i][pCoord.getY()].cambiarEstado("Barco");;
-				}
-				else {
-					pTablero[pCoord.getX()][pCoord.getY()-i].cambiarEstado("Barco");;
-				}
+		boolean colocado=true;
+	
+		for (int i=0;i<3; i++) {
+			if (pHorizontal) {
+				pTablero[pCoord.getX()+i][pCoord.getY()].cambiarEstado("Barco");;
+			}
+			else {
+				pTablero[pCoord.getX()][pCoord.getY()+i].cambiarEstado("Barco");;
 			}
 		}
-
+		return colocado; 
 		
 	}
 
