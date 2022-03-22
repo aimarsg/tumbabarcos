497b310ab4d42db309fb04e78da70285f38f174e
aimarsg <aimar.s.737@gmail.com>
2022-03-22 11:28:56 +0100

    portaaviones

--- src/packModelo/PortaAviones.java
+++ src/packModelo/PortaAviones.java
@@ -1,9 +1,19 @@
 package packModelo; 
 public class PortaAviones extends Barco {
-
-	public void colocarBarco() {
-		// TODO - implement PortaAviones.colocarBarco
-		throw new UnsupportedOperationException();
+	//longitud del portaaviones = 4
+	public boolean colocarBarco(Coordenada pCoordenada, boolean pHorizontal, Casilla[][] tablero) {
+		int i = pCoordenada.getX();
+		int j = pCoordenada.getY();
+		int cont = 0;
+		while (cont < 4) {
+			tablero[i][j].cambiarEstado("Barco");
+			if (pHorizontal) {
+				i++;
+			}else {
+				j++;
+			}
+			cont++;
+		}
 	}
 
 }
