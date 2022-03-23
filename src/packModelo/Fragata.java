198cc2374ffc1b707cba8ee7d190b4d849143271
Mario <Mario@10.57.2.42>
2022-03-23 18:37:04 +0100

    fragata restricciones

--- src/packModelo/Fragata.java
+++ src/packModelo/Fragata.java
@@ -8,8 +8,33 @@ public class Fragata extends Barco {
 	public boolean colocarBarco(Coordenada pCoordenada, boolean pHorizontal, Casilla[][] pTablero) {
 		int x= pCoordenada.getX();
 		int y= pCoordenada.getY();
-	
-		pTablero[x][y].cambiarEstado("Barco");
+		boolean posible=true;
+		Casilla casilla= pTablero[x][y];
+		if((x>-1 && x<11) && (y>-1 && y<11)) {
+			if(casilla.combrobarEstado()!="Agua") {
+				if(pTablero[x+1][y].combrobarEstado()!="Agua"&&
+					pTablero[x][y+1].combrobarEstado()!="Agua"&&
+					pTablero[x-1][y].combrobarEstado()!="Agua"&&
+					pTablero[x][y-1].combrobarEstado()!="Agua"&&
+					pTablero[x+1][y+1].combrobarEstado()!="Agua"&&
+					pTablero[x-1][y+1].combrobarEstado()!="Agua"&&
+					pTablero[x-1][y-1].combrobarEstado()!="Agua"&&
+					pTablero[x+1][y-1].combrobarEstado()!="Agua") {
+					pTablero[x][y].cambiarEstado("Barco");
+				}else {
+					posible= false;
+					System.out.println("El Barco no esta completamente rodeado de agua");
+				}
+			}else {
+				posible= false;
+				System.out.println("la casilla seleccionada no es agua");
+			}
+			
+		}else {
+			posible= false;
+			System.out.println("Las coordenadas no estan en el tablero");
+		}
+		
 		return (Boolean) null;
 		
 	}
